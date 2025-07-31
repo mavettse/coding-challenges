b2418a5046e390a37d5f1aac5c56792a68089731
Giovanni Estevam <gioestevam04@gmail.com>
2025-07-31 15:31:05 -0300

    feat: Add Solution 013 - Roman to Integer from LeetCode

---
+++ leetcode/java/src/p070_climbingstairs/Solution.java
@@ -0,0 +1,19 @@
+package p070_climbingstairs;
+
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 1)
+            return 1;
+
+        int passo1Ant = 2;
+        int passo2Ant = 1;
+
+        for (int i = 3; i <= n; ++i) {
+            int passoAtual = passo1Ant + passo2Ant;
+            passo2Ant = passo1Ant;
+            passo1Ant = passoAtual;
+        }
+
+        return passo1Ant;
+    }
+}
