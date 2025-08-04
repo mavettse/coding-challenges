31ebc99263ea5e716c81ce4f535cd7b3e6239b34
Giovanni Estevam <gioestevam04@gmail.com>
2025-08-04 16:58:07 -0300

    feat: Add question 169 'Majority Element' from LeetCode (2nd version - using the Boyer-Moore Voting Algorithm)

--- leetcode/java/src/p169_majorityelement/Solution.java
+++ leetcode/java/src/p169_majorityelement/Solution.java
@@ -1,27 +1,18 @@
 package p169_majorityelement;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        int number = nums[0];
-        Map<Integer, Integer> elements = new HashMap<>();
+        int count = 0;
+        int candidate = 0;
 
-        for(int i = 0; i < n; ++i){
-            if(elements.containsKey(nums[i])){
-                elements.put(nums[i], elements.get(nums[i]) + 1);
-            } else {
-                elements.put(nums[i], 1);
+        for(int num : nums){
+            if(count == 0){
+                candidate = num;
             }
 
-            if(elements.get(nums[i]) > n/2){
-                number = nums[i];
-                break;
-            }
+            count += (candidate == num) ? 1 : -1;
         }
 
-        return number;
+        return candidate;
     }
 }
