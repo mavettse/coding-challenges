3aab5a187a25e2b23086422bb7108a3784458473
Giovanni Estevam <gioestevam04@gmail.com>
2025-08-04 15:07:32 -0300

    feat: Add question 136 'Single Number' from LeetCode

---
+++ leetcode/java/src/p136_singlenumber/Solution.java
@@ -0,0 +1,13 @@
+package p136_singlenumber;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int number = 0;
+
+        for(int num : nums){
+            number = number ^ num;
+        }
+
+        return number;
+    }
+}
