2340707a46acb4ee64d9b6131e11cbc49ccf4a33
Giovanni Estevam <gioestevam04@gmail.com>
2025-08-05 19:22:07 -0300

    feat: Add question 219 'Contains Duplicate' from LeetCode (2nd version using Sliding Window algorithm)

--- leetcode/java/src/p219_containsduplicateii/Solution.java
+++ leetcode/java/src/p219_containsduplicateii/Solution.java
@@ -1,19 +1,24 @@
 package p219_containsduplicateii;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         if(nums == null || nums.length <= 1){
             return false;
         }
 
-        Map<Integer, Integer> numsTester = new HashMap<>();
+        Set<Integer> window = new HashSet<>();
 
         for(int i = 0; i < nums.length; ++i){
-            if(numsTester.containsKey(nums[i]) && Math.abs(i - numsTester.get(nums[i])) <= k){
-                return true;
+            if(i > k){
+                window.remove(nums[i - k - 1]);
             }
 
-            numsTester.put(nums[i], i);
+            if(!window.add(nums[i])){
+                return true;
+            }
         }
 
         return false;
