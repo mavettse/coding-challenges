c1c453803903a2a507bab24c60494a6e0a564a46
Giovanni <gioestevam04@gmail.com>
2025-08-17 21:58:01 -0300

    feat(026): Add Remove Duplicates from Sorted Array C++ solution

---
+++ leetcode/cpp/array/p026_removeduplicates.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 0;
+
+        for(int i = 1; i < nums.size(); ++i){
+            if(nums[k] != nums[i]){
+                nums[++k] = nums[i];
+            }
+        }
+
+        return k+1;
+    }
+};
